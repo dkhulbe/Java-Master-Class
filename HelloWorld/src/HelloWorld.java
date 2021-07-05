c87722002de00f27333181bc4f277e325c4ad8e0
dkhulbe <firstbournxd@gmail.com>
2021-07-05 14:23:17 -0700

    stuff

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        System.out.println("Hello, Dhananjay!");
+
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLast = 1000- myTotal;
+        System.out.println(myTotal);
+        System.out.println(myLast);
     }
 }
