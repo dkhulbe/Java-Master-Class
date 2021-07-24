a6fee38ea61821a3eb10e61439051afbebd2f57a
dkhulbe <firstbournxd@gmail.com>
2021-07-23 17:23:07 -0700

    message

---
+++ Counter/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int count = 0; count <=100; count++){
+            System.out.println(count);
+            if (count == 50)
+                System.out.println("halfway there");
+        }
+    }
+}
